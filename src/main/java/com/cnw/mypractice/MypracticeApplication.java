163a140224da46713a8bd167c44c8b659170c44c
HOME <coderiding9060@gmail.com>
2021-10-12 06:58:08 +0900

    Initial commit

---
+++ src/main/java/com/cnw/mypractice/MypracticeApplication.java
@@ -0,0 +1,13 @@
+package com.cnw.mypractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypracticeApplication.class, args);
+	}
+
+}
