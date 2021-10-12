324cc6173f24a0e044c759e032fa332e87a03a99
HOME <coderiding9060@gmail.com>
2021-10-12 14:31:49 +0900

    <feat><BoardRepository.java> 스프링 데이터 JPA를 위한 Dao 추가

---
+++ src/main/java/com/cnw/mypractice/repository/BoardRepository.java
@@ -0,0 +1,8 @@
+package com.cnw.mypractice.repository;
+
+import com.cnw.mypractice.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
